69a6ff907918e7d4f8a0eb223e397e1e56218c3e
Rocio Acosta <121262117+rooacost@users.noreply.github.com>
2023-04-29 01:08:09 -0300

    se agrego modo nocturno, una barra de busqueda con logica y ademas el buscador

---
+++ js/index.js
@@ -0,0 +1,82 @@
+// buscador de contenido
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+
+let bars_search = document.getElementById("ctn-bars-search");
+let cover_ctn_search = document.getElementById("cover-ctn-search");
+let inputSearch = document.getElementById("inputSearch");
+let box_search = document.getElementById("box-search");
+
+
+function mostrar_buscador(){
+
+  bars_search.style.top = "50px";
+  cover_ctn_search.style.display = "block";
+  inputSearch.focus();
+
+}
+
+function ocultar_buscador(){
+
+  bars_search.style.top = "-25px";
+  cover_ctn_search.style.display = "none";
+  inputSearch.value = "";
+  box_search.style.display = "none";
+
+}
+
+
+// // Filtro de busqueda
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno(){
+
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+
+        }else{
+            li[i].style.display = "none";
+        }
+
+    }
+
+
+}
+
+
+// modo oscuro
+
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const slider = document.getElementById ('slider_dark');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute ('data-theme', theme);
+    localStorage.setItem ('theme',theme);
+}
+slider.addEventListener('click', () => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme (switchToTheme);
+}
+);
+
+setTheme(localStorage.getItem ('theme') || preferedColorScheme);
+
+
+
+
---
+++ js/index_2.js
@@ -0,0 +1,16 @@
+// // const searchInput = document.querySelector("#buscador");
+// // const products = document.querySelectorAll(".product-item");
+
+// // searchInput.addEventListener("input", () => {
+// //   const searchTerm = searchInput.value.toLowerCase();
+  
+// //   products.forEach((product) => {
+// //     const title = product.querySelector("h3").textContent.toLowerCase();
+    
+// //     if (title.includes(searchTerm)) {
+// //       product.style.display = "block";
+// //     } else {
+// //       product.style.display = "none";
+// //     }
+// //   });
+// // });
