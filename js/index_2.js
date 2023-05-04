85ca6014567355697bb8d6ff10b3f2c3ed63b5da
Yohana Valdez <yohana.valdez@hotmail.com>
2023-05-03 22:40:53 -0300

    cambios

--- js/index_2.js
+++ js/index_2.js
@@ -1,16 +1,16 @@
-// // const searchInput = document.querySelector("#buscador");
-// // const products = document.querySelectorAll(".product-item");
+// const searchInput = document.querySelector("#buscador");
+// const products = document.querySelectorAll(".product-item");
 
-// // searchInput.addEventListener("input", () => {
-// //   const searchTerm = searchInput.value.toLowerCase();
+// searchInput.addEventListener("input", () => {
+//   const searchTerm = searchInput.value.toLowerCase();
   
-// //   products.forEach((product) => {
-// //     const title = product.querySelector("h3").textContent.toLowerCase();
+//   products.forEach((product) => {
+//     const title = product.querySelector("h3").textContent.toLowerCase();
     
-// //     if (title.includes(searchTerm)) {
-// //       product.style.display = "block";
-// //     } else {
-// //       product.style.display = "none";
-// //     }
-// //   });
-// // });
+//     if (title.includes(searchTerm)) {
+//       product.style.display = "block";
+//     } else {
+//       product.style.display = "none";
+//     }
+//   });
+// });
